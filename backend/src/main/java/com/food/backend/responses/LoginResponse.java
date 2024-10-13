1bfbf58b3ccc43d5acaee8a02cef9adc48c4a0b7
BlackSpy <48418515+blackspyek@users.noreply.github.com>
2024-10-13 14:32:21 +0200

    Feat/roles in login response Now roles are returned in a response when user is logged in

--- backend/src/main/java/com/food/backend/responses/LoginResponse.java
+++ backend/src/main/java/com/food/backend/responses/LoginResponse.java
@@ -1,16 +1,23 @@
 package com.food.backend.responses;
 
+import com.food.backend.model.Role;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
 public class LoginResponse {
     private String token;
     private long expiresIn;
+    private Set<Role> roles;
 
-    public LoginResponse(String token, long expiresIn) {
+    public LoginResponse(String token, long expiresIn, Set<Role> roles) {
         this.token = token;
         this.expiresIn = expiresIn;
+        this.roles = roles;
+
     }
 }
+
