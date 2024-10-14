9a53c4aed873da0840fa6ed2bbde76243ef6ea6c
blackspyek <blackspytv@gmail.com>
2024-10-14 19:28:42 +0200

    fix/fixed endpoint fixed endpoint

--- backend/src/main/java/com/food/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/food/backend/config/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
